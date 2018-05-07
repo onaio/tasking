edc00e5a3e8c5de057cc6d0610e86d62c8a423d3
Ona KE code machine <tech+ke@ona.io>
2018-05-07 14:48:04 +0300

    Cleanup code
    - Make flake8 and pylint tests pass
    - Use manage.py for tests
    - Add tests/settings.py
    - Add .travis.yml
    
    Co-authored-by: Kelvin Jayanoris <kjayanoris@gmail.com>
    Co-authored-by: Njagi Mwaniki <njagi@urbanslug.com>
    Co-authored-by: Davis Raymond  <davisraymondmuro@outlook.com>

--- tasking/apps.py
+++ tasking/apps.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
+"""
+Apps module for tasking app
+"""
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
 
 
 class TaskingConfig(AppConfig):
+    """
+    Tasking App Config Class
+    """
     name = 'tasking'
