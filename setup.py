8c2a5557634247bc6a7805fe3bbd08cc4bffedef
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-05-07 19:57:42 +0300

    Use setup.py install_requires

--- setup.py
+++ setup.py
@@ -6,11 +6,15 @@ from setuptools import setup
 setup(
     name='ona-tasking',
     version='0.1',
-    description='A tasking library.',
+    description='A Django app that provides adds tasking to your Django '
+                'project.',
     license='GPL 3',
     author='Ona Kenya',
     author_email='tech@ona.io',
     url='https://github.com/onaio/tasking',
+    install_requires=[
+        'Django >= 1.11',
+    ],
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
