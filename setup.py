934fce333fb9ffe5c1d627aa0a9374a889f9ed5a
Ona KE code machine <tech+ke@ona.io>
2018-05-07 12:30:35 +0300

    Added Setup Files.
    Co-authored-by: Kelvin Jayanoris <kjayanoris@gmail.com>
    Co-authored-by: Njagi Mwaniki <njagi@urbanslug.com>
    Co-authored-by: Davis Raymond  <davisraymondmuro@outlook.com>

---
+++ setup.py
@@ -0,0 +1,26 @@
+"""
+Setup.py for ona tasking
+"""
+from setuptools import setup
+
+setup(
+    name='ona-tasking',
+    version='0.1',
+    description='A tasking library.',
+    license='MIT',
+    author='Ona Kenya',
+    author_email='tech@ona.io',
+    url='https://github.com/onaio/tasking',
+    install_requires=[
+        'django',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+    ],
+)
+
