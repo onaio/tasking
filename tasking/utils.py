216b66d02c28f32e901fedf3580c9bb8bb212ee5
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-05-08 18:29:52 +0300

    Add custom exception to utils
    Co-authored-by: Kelvin Jayanoris <kjayanoris@gmail.com>
    Co-authored-by: Njagi Mwaniki <njagi@urbanslug.com>
    Co-authored-by: Davis Raymond  <davisraymondmuro@outlook.com>

--- tasking/utils.py
+++ tasking/utils.py
@@ -4,7 +4,10 @@ Utility functions for tasking
 """
 from __future__ import unicode_literals
 
+from django.contrib.contenttypes.models import ContentType
 from dateutil.rrule import rrulestr
+
+from tasking.common_tags import TARGET_DOES_NOT_EXIST
 
 
 def validate_rrule(rule_string):
@@ -21,3 +24,21 @@ def validate_rrule(rule_string):
         return False
     else:
         return True
+
+
+class TargetDoesNotExist(Exception):
+    """
+    Custom Exception raised when the target type does not exist
+    """
+
+    message = TARGET_DOES_NOT_EXIST
+
+
+def get_target(app_label, target_type):
+    """
+    Returns the target_type
+    """
+    try:
+        return ContentType.objects.get(app_label=app_label, model=target_type)
+    except ContentType.DoesNotExist:
+        raise TargetDoesNotExist(TARGET_DOES_NOT_EXIST)
