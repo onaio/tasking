918ccbc85ea764210475cef4ee3b3bfa63391ce3
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-05-08 15:17:19 +0300

    Cleanup BaseTask Add generic foreign key to `target`
    Co-authored-by: Kelvin Jayanoris <kjayanoris@gmail.com>
    Co-authored-by: Njagi Mwaniki <njagi@urbanslug.com>
    Co-authored-by: Davis Raymond  <davisraymondmuro@outlook.com>

--- tasking/models/tasks.py
+++ tasking/models/tasks.py
@@ -6,6 +6,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.utils.translation import ugettext as _
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.utils.encoding import python_2_unicode_compatible
 
 
@@ -23,9 +25,15 @@ class BaseTask(models.Model):
     STATUS_CHOICES = (
         (ACTIVE, _('Active')),
         (DRAFT, _('Draft')),
-        (CLOSED, 'Closed'),
+        (CLOSED, _('Closed')),
     )
 
+    created = models.DateTimeField(
+        varbose_name=_('Created'),
+        auto_now_add=True)
+    modified = models.DateTimeField(
+        verbose_name=_('Modified'),
+        auto_now=True)
     parent = models.ForeignKey(
         'self',
         verbose_name=_('Parent task'),
@@ -43,7 +51,7 @@ class BaseTask(models.Model):
         blank=True,
         default='',
         help_text=_('This represents the description.'))
-    tasklist_id = models.ForeignKey(
+    tasklist = models.ForeignKey(
         'tasking.BaseTaskList',
         verbose_name=_('Task List'),
         null=True,
@@ -51,7 +59,7 @@ class BaseTask(models.Model):
         default=None,
         on_delete=models.SET_NULL,
         help_text=_('This represents the tasklist.'))
-    location_id = models.ForeignKey(
+    location = models.ForeignKey(
         'tasking.BaseLocation',
         verbose_name=_('Location'),
         null=True,
@@ -59,6 +67,25 @@ class BaseTask(models.Model):
         default=None,
         on_delete=models.SET_NULL,
         help_text=_('This represents the location.'))
+    # we use Django's ContentType app to add a Generic Foreign Key
+    # this makes it possible to tie a Task to any other model
+    # which is the `target`
+    target_content_type = models.ForeignKey(
+        ContentType,
+        related_name='target',
+        blank=True,
+        null=True,
+        default=None,
+        db_index=True,
+        on_delete=models.SET_NULL)
+    target_object_id = models.PositiveIntegerField(
+        db_index=True,
+        blank=True,
+        null=True,
+        default=None
+    )
+    target_content_object = GenericForeignKey(
+        'target_content_type', 'target_object_id')
     start = models.DateTimeField(
         verbose_name=_('Start'),
         help_text=_('This is the date and time the task starts.')
@@ -109,7 +136,22 @@ class Task(BaseTask):
     """
     Task model class
     """
-    pass
+    tasklist = models.ForeignKey(
+        'tasking.TaskList',
+        verbose_name=_('Task List'),
+        null=True,
+        blank=True,
+        default=None,
+        on_delete=models.SET_NULL,
+        help_text=_('This represents the tasklist.'))
+    location = models.ForeignKey(
+        'tasking.Location',
+        verbose_name=_('Location'),
+        null=True,
+        blank=True,
+        default=None,
+        on_delete=models.SET_NULL,
+        help_text=_('This represents the location.'))
 
     # pylint: disable=no-self-use
     class Meta(object):
