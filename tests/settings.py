4e5ecd389f26f43ce029c7cf0f6dd7629aafeed3
DavisRayM <davisraymondmuro@outlook.com>
2018-05-10 00:40:22 +0300

    Use PostGres for Tests

--- tests/settings.py
+++ tests/settings.py
@@ -8,18 +8,28 @@ from __future__ import unicode_literals
 INSTALLED_APPS = (
     # core django apps
     'django.contrib.contenttypes',
-    'django.contrib.gis',  # Need for the Location model.
+    'django.contrib.gis',
     # third party
     'rest_framework',
     # custom
     'tasking',
 )
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
-        'NAME': ':memory:',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'ona_tasking',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1'
     }
 }
 SECRET_KEY = "secret_key_for_testing"
-SPATIALITE_LIBRARY_PATH = 'mod_spatialite'
-# GDAL_LIBRARY_PATH = '/usr/lib/ogdi/libgdal.so'
+
+# try and load local_settings if present
+try:
+    # pylint: disable=wildcard-import
+    # pylint: disable=unused-wildcard-import
+    from .local_settings import *  # noqa
+except ImportError:
+    pass
