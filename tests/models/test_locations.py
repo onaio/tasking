33b848d7e68474f6d27ceed6bb8b9f413555441e
DavisRayM <davisraymondmuro@outlook.com>
2018-05-09 18:16:21 +0300

    Uncommented Location model and Test

--- tests/models/test_locations.py
+++ tests/models/test_locations.py
@@ -1,23 +1,23 @@
-# # -*- coding: utf-8 -*-
-# """
-# Test for Location model
-# """
-# from __future__ import unicode_literals
-#
-# from django.test import TestCase
-#
-# from model_mommy import mommy
-#
-#
-# class TestLocations(TestCase):
-#     """
-#     Test class for Location models
-#     """
-#
-#     def test_task_model_str(self):
-#         """
-#         Test the str method on Location model
-#         """
-#         nairobi = mommy.make('tasking.Location', name="Nairobi")
-#         expected = '{} - Nairobi'.format(nairobi.country)
-#         self.assertEqual(expected, nairobi.__str__())
+# -*- coding: utf-8 -*-
+"""
+Test for Location model
+"""
+from __future__ import unicode_literals
+
+from django.test import TestCase
+
+from model_mommy import mommy
+
+
+class TestLocations(TestCase):
+    """
+    Test class for Location models
+    """
+
+    def test_task_model_str(self):
+        """
+        Test the str method on Location model
+        """
+        nairobi = mommy.make('tasking.Location', name="Nairobi")
+        expected = '{} - Nairobi'.format(nairobi.country)
+        self.assertEqual(expected, nairobi.__str__())
