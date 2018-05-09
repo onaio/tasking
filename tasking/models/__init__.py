04989d2b4d823505c01cb615f482c24aa244bed3
Njagi Mwaniki <njagi@urbanslug.com>
2018-05-09 11:19:24 +0300

    Added a Task List model
    Holds a list of tasks.
    The task list model has a many to many relationship to the tasks model.
    
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

--- tasking/models/__init__.py
+++ tasking/models/__init__.py
@@ -4,4 +4,5 @@ Task Models init module
 """
 from __future__ import unicode_literals
 
-from tasking.models.tasks import BaseTask, Task  # noqa
+from tasking.models.tasks import BaseTask, Task # noqa
+from tasking.models.task_list import BaseTaskList, TaskList
---
+++ tasking/models/task_list.py
@@ -0,0 +1,45 @@
+"""
+Module for the Task List model
+"""
+
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext as _
+
+class BaseTaskList(models.Model):
+    """
+    Base abstract model class for a Task List
+
+    This class is meant to be extended to add Task Lists to your own project.
+    It only implements the bare minimum of what a Task List could be.
+    """
+    name = models.CharField(
+      _('Name'),
+      max_length=255,
+      help_text=_('This is the name of the task list')
+    )
+
+    class Meta(object):
+        """
+        The meta option class for BaseTaskList
+        """
+        abstract = True
+@python_2_unicode_compatible
+class TaskList(BaseTaskList):
+
+    class Meta(object):
+        """
+        This is the meta options class for the Task List model
+        """
+        abstract = False
+        ordering = ['name']
+
+    def __str__(self):
+        """
+        String representation of a Task List object
+
+        e.g. Cow prices - 1
+        """
+        return self.name
