646da83c398af72bc6a7c2b00fba7de73a571d6b
DavisRayM <davisraymondmuro@outlook.com>
2018-05-10 10:16:57 +0300

    Add SegmentRule Model

--- tasking/models/__init__.py
+++ tasking/models/__init__.py
@@ -5,7 +5,5 @@ Task Models init module
 from __future__ import unicode_literals
 
 from tasking.models.tasks import BaseTask, Task  # noqa
-
 from tasking.models.segment_rules import BaseSegmentRule, SegmentRule  # noqa
-
 from tasking.models.locations import BaseLocation, Location  # noqa
