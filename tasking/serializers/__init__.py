6441d52b6003c8c9d189ecda27423e894268341e
Njagi Mwaniki <njagi@urbanslug.com>
2018-05-10 17:47:51 +0300

    Remove _serializers suffix on serializer src files
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

--- tasking/serializers/__init__.py
+++ tasking/serializers/__init__.py
@@ -4,5 +4,5 @@ Task Serializers init module
 """
 from __future__ import unicode_literals
 
-from tasking.serializers.task_serializers import TaskSerializer  # noqa
-from tasking.serializers.task_list_serializers import TaskListSerializer  # noqa
+from tasking.serializers.task import TaskSerializer  # noqa
+from tasking.serializers.task_list import TaskListSerializer  # noqa
