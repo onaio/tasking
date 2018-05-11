d0acd97282cfeab1496eddce889c614d9c4ccaaa
DavisRayM <davisraymondmuro@outlook.com>
2018-05-11 12:40:51 +0300

    Add validation for geopoint and radius or shapefile entry

--- tasking/serializers/location_serializers.py
+++ tasking/serializers/location_serializers.py
@@ -6,6 +6,8 @@ from __future__ import unicode_literals
 
 from rest_framework import serializers
 
+from tasking.common_tags import RADIUS_MISSING, GEODETAILS_ONLY
+from tasking.common_tags import GEOPOINT_MISSING
 from tasking.models import Location
 
 
@@ -13,6 +15,37 @@ class LocationSerializer(serializers.ModelSerializer):
     """
     Location serializer class
     """
+
+    def validate(self, attrs):
+        """
+        Custom Validation for Location Serializer
+        """
+
+        geopoint = attrs.get('geopoint')
+        radius = attrs.get('radius')
+        shapefile = attrs.get('shapefile')
+
+        if geopoint is not None:
+            if shapefile is not None:
+                raise serializers.ValidationError(
+                    {'shapefile': GEODETAILS_ONLY}
+                )
+            if radius is None:
+                raise serializers.ValidationError(
+                    {'radius': RADIUS_MISSING}
+                )
+        if radius is not None:
+            if shapefile is not None:
+                raise serializers.ValidationError(
+                    {'shapefile': GEODETAILS_ONLY}
+                )
+            if geopoint is None:
+                raise serializers.ValidationError(
+                    {'geopoint': GEOPOINT_MISSING}
+                )
+
+        return super(LocationSerializer, self).validate(attrs)
+
     # pylint: disable=too-few-public-methods
     class Meta(object):
         """
