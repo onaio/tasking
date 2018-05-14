f7195de47fedf51e623d243a3039bfb7be5cbfdf
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-05-14 11:19:37 +0300

    Update licence

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version='0.1',
     description='A Django app that provides adds tasking to your Django '
     'project.',
-    license='GPL 3',
+    license='Apache 2.0',
     author='Ona Kenya',
     author_email='tech@ona.io',
     url='https://github.com/onaio/tasking',
