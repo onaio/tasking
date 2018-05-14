2bc6d0ff82896905a7f8d7ec1f8d2fc9628fa1ce
DavisRayM <davisraymondmuro@outlook.com>
2018-05-14 15:46:32 +0300

    Add Tests for ProjectSerializer

--- tests/serializers/test_projects.py
+++ tests/serializers/test_projects.py
@@ -4,36 +4,66 @@ Test for Project Serializers
 """
 from __future__ import unicode_literals
 
-from django.test import TestCase
+from collections import OrderedDict
 
 from model_mommy import mommy
+from tests.serializers.test_base import TestSerializerBase
 
 from tasking.serializers import ProjectSerializer
 
 
-class TestProjectSerializer(TestCase):
+class TestProjectSerializer(TestSerializerBase):
     """
     Test the Project Serializer
     """
-    def test_create_Project(self):
+
+    def test_validate_bad_data(self):
+        """
+        Test that ProjectSerializer validate works as expected
+        for bad data.
+        """
+        mocked_target_object = mommy.make('tasking.Task')
+
+        bad_target_id = OrderedDict(
+            name='Livestock Prices',
+            target_content_type=self.task_type.id,
+            target_object_id=1337,
+        )
+
+        self.assertFalse(ProjectSerializer(data=bad_target_id).is_valid())
+
+        bad_content_type = OrderedDict(
+            name='Livestock Prices',
+            target_content_type='foobar',
+            target_object_id=mocked_target_object.id,
+        )
+
+        self.assertFalse(ProjectSerializer(data=bad_content_type).is_valid())
+
+    def test_create_project(self):
         """
         Test that the serializer can create Project objects
         """
 
         task_1 = mommy.make('tasking.Task')
         task_2 = mommy.make('tasking.Task')
+        mocked_target_object = mommy.make('tasking.Task')
 
-        data = {'name': "Livestock prices"}
+        data = {
+            'name': "Livestock prices",
+            'target_content_type': self.task_type.id,
+            'target_id': mocked_target_object.id,
+        }
+
         data_with_tasks = data.copy()
         data_with_tasks['tasks'] = [task_1.id, task_2.id]
 
         serializer_instance = ProjectSerializer(data=data_with_tasks)
-
         self.assertTrue(serializer_instance.is_valid())
-        Project = serializer_instance.save()
+        project = serializer_instance.save()
 
         self.assertDictContainsSubset(data, serializer_instance.data)
-        self.assertEqual('Livestock prices', Project.name)
+        self.assertEqual('Livestock prices', project.name)
 
         self.assertEqual(set([task_1.id, task_2.id]),
                          set(serializer_instance.data['tasks']))
@@ -43,7 +73,9 @@ class TestProjectSerializer(TestCase):
             'name',
             'tasks',
             'created',
-            'modified'
+            'modified',
+            'target_content_type',
+            'target_id',
         ]
 
         self.assertEqual(set(expected_fields),
