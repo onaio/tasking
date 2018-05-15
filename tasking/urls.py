400c886e6b0394927cfdcf1abc636956181fd536
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-05-15 15:53:28 +0300

    Add projectViewSet to urls

--- tasking/urls.py
+++ tasking/urls.py
@@ -5,10 +5,11 @@ Tasking urls module.
 from django.conf.urls import include, url
 from rest_framework import routers
 
-from tasking.viewsets import TaskViewSet
+from tasking.viewsets import TaskViewSet, ProjectViewSet
 
 router = routers.DefaultRouter(trailing_slash=False)  # pylint: disable=C0103
 router.register(r'tasks', TaskViewSet)
+router.register(r'tasks', ProjectViewSet)
 
 urlpatterns = [  # pylint: disable=C0103
     url(r'^api/v1/', include(router.urls)),
