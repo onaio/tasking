b9af824cf585d08342fdd006fbe195b322a0219b
DavisRayM <davisraymondmuro@outlook.com>
2018-05-15 15:44:13 +0300

    Add validation for task

--- tasking/serializers/submissions.py
+++ tasking/serializers/submissions.py
@@ -4,14 +4,31 @@ Submission Serializers
 """
 from __future__ import unicode_literals
 
+from rest_framework import serializers
+
 from tasking.models import Submission
 from tasking.serializers.base import GenericForeignKeySerializer
+from tasking.common_tags import CANT_EDIT_TASK
 
 
 class SubmissionSerializer(GenericForeignKeySerializer):
     """
     Submission serializer class
     """
+
+    def validate_task(self, value):
+        """
+        Validate Task
+        """
+        if self.instance is not None:
+            if self.instance.task is value:
+                return value
+            else:
+                raise serializers.ValidationError(
+                    CANT_EDIT_TASK
+                )
+        else:
+            return value
 
     # pylint: disable=too-few-public-methods
     class Meta(object):
