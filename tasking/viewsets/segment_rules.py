40acd2f86bbae738dd4c70b6f24e6a2c30b6b0c7
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-05-15 15:30:19 +0300

    Add SegmentRuleViewSet

---
+++ tasking/viewsets/segment_rules.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+SegmentRule viewsets
+"""
+from __future__ import unicode_literals
+
+from rest_framework import mixins, viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from tasking.models import SegmentRule
+from tasking.serializers import SegmentRuleSerializer
+
+
+# pylint: disable=too-many-ancestors
+class SegmentRuleViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
+                         mixins.RetrieveModelMixin, mixins.DestroyModelMixin,
+                         mixins.UpdateModelMixin, viewsets.GenericViewSet):
+    """
+    Viewset for tasks
+    """
+    serializer_class = SegmentRuleSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = SegmentRule.objects.all()  # pylint: disable=no-member
