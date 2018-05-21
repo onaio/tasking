9d6af24a0296762f61ed780acae666dc2c967443
DavisRayM <davisraymondmuro@outlook.com>
2018-05-21 18:08:11 +0300

    Add get_shpname custom method

--- tasking/utils.py
+++ tasking/utils.py
@@ -4,12 +4,15 @@ Utility functions for tasking
 """
 from __future__ import unicode_literals
 
+import zipfile
+
 from django.contrib.contenttypes.models import ContentType
 
 from django.utils import timezone
 from dateutil.rrule import rrulestr
 
-from tasking.exceptions import TargetDoesNotExist
+from tasking.exceptions import TargetDoesNotExist, ShapeFileNotFound
+from tasking.exceptions import MissingFiles, UnnecessaryFiles
 
 
 def validate_rrule(rule_string):
@@ -71,3 +74,24 @@ def get_target(app_label, target_type):
         return ContentType.objects.get(app_label=app_label, model=target_type)
     except ContentType.DoesNotExist:  # pylint: disable=no-member
         raise TargetDoesNotExist()
+
+
+def get_shpname(geofile):
+    """
+    Returns the filename of ShapeFile
+    """
+    names = geofile.namelist()
+    name = None
+
+    if len(names) > 3:
+        raise UnnecessaryFiles()
+    if len(names) == 3:
+        for x in names:
+            file_shp = x.split('.')
+            if file_shp[1] == 'shp':
+                return ".".join(file_shp)
+    else:
+        raise MissingFiles()
+
+    if name is None:
+        raise ShapeFileNotFound()
