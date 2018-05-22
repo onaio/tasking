971084679cf159811f44d9dfd2ce3e401988e412
DavisRayM <davisraymondmuro@outlook.com>
2018-05-22 18:59:01 +0300

    Fix Pylint error in ContentTypeSerializer

--- tasking/serializers/contenttype.py
+++ tasking/serializers/contenttype.py
@@ -7,6 +7,7 @@ from __future__ import unicode_literals
 from rest_framework import serializers
 
 
+# pylint: disable=abstract-method
 class ContentTypeSerializer(serializers.Serializer):
     """
     Serializer for ContentType ViewSet
