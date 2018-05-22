37220051822a64114fc6fb8883d47adc2295b423
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-05-22 16:02:25 +0300

    Add TaskOccurrenceSerializer and tests

---
+++ tasking/serializers/occurrence.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+TaskOccurrence Serializers
+"""
+
+from __future__ import unicode_literals
+
+from rest_framework import serializers
+
+from tasking.models import TaskOccurrence
+
+
+class TaskOccurrenceSerializer(serializers.ModelSerializer):
+    """
+    TaskOccurrence serializer class
+    """
+    time_string = serializers.SerializerMethodField()
+
+    # pylint: disable=too-few-public-methods
+    class Meta(object):
+        """
+        Meta options for TaskOccurrenceSerializer
+        """
+        model = TaskOccurrence
+        fields = [
+            'id',
+            'task',
+            'created',
+            'modified',
+            'date',
+            'start_time',
+            'end_time',
+            'time_string'
+        ]
+
+    # pylint: disable=no-self-use
+    def get_time_string(self, obj):
+        """
+        Returns a friendly human-readable description of the occurrence
+        date and times
+        """
+        return obj.get_timestring()
