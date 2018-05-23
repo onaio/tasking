d9817becf083a3a85df74790152a7168e1ac56a9
DavisRayM <davisraymondmuro@outlook.com>
2018-05-23 10:43:17 +0300

    Modify ContentTypeSerializer - Add Meta Options for Serializer - Add app_label, model and id fields

--- tasking/serializers/contenttype.py
+++ tasking/serializers/contenttype.py
@@ -6,10 +6,25 @@ from __future__ import unicode_literals
 
 from rest_framework import serializers
 
+from django.contrib.contenttypes.models import ContentType
+
 
 # pylint: disable=abstract-method
 class ContentTypeSerializer(serializers.Serializer):
     """
     Serializer for ContentType ViewSet
     """
-    name = serializers.CharField(read_only=True, max_length=255)
+    app_label = serializers.CharField(max_length=255)
+    model = serializers.CharField(max_length=255)
+    id = serializers.IntegerField()
+
+    class Meta(object):
+        """
+        Meta Options for ContentTypeSerializer
+        """
+        model = ContentType
+        fields = [
+            'app_label',
+            'model',
+            'id'
+        ]
