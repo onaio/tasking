de1627a8532c70bd448d463d307bcacf69a4374f
DavisRayM <davisraymondmuro@outlook.com>
2018-05-23 11:31:25 +0300

    Modify Tests in TestContentTypeViewSet

--- tests/viewsets/test_contenttype.py
+++ tests/viewsets/test_contenttype.py
@@ -4,7 +4,8 @@ Tests ContentType viewsets.
 """
 from __future__ import unicode_literals
 
-from collections import OrderedDict
+from django.contrib.contenttypes.models import ContentType
+
 from model_mommy import mommy
 from rest_framework.test import APIRequestFactory, force_authenticate
 from tests.base import TestBase
@@ -28,20 +29,11 @@ class TestContentTypeViewSet(TestBase):
         user = mommy.make('auth.User')
         view = ContentTypeViewSet.as_view({'get': 'list'})
 
-        expected_data = [
-            OrderedDict([('app_label', 'auth'), ('model', 'group'), ('id', 2)]),
-            OrderedDict([('app_label', 'auth'), ('model', 'user'), ('id', 3)]),
-            OrderedDict([('app_label', 'tasking'), ('model', 'location'), ('id', 5)]),
-            OrderedDict([('app_label', 'tasking'), ('model', 'project'), ('id', 6)]),
-            OrderedDict([('app_label', 'tasking'), ('model', 'segmentrule'), ('id', 7)]),
-            OrderedDict([('app_label', 'tasking'), ('model', 'submission'), ('id', 8)]),
-            OrderedDict([('app_label', 'tasking'), ('model', 'task'), ('id', 9)])
-        ]
-
         request = self.factory.get('/contenttypes')
         force_authenticate(request, user=user)
         response = view(request=request)
 
         self.assertEqual(response.status_code, 200)
-        self.assertListEqual(
-            expected_data, response.data)
+        self.assertEqual(len(response.data), 7)
+        self.assertTrue(
+            ContentType.objects.filter(pk=response.data[1]['id']).exists())
