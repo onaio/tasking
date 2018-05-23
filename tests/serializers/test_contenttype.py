34c6d45a43ff9fe45b88ca59b7fe9274b9da1648
DavisRayM <davisraymondmuro@outlook.com>
2018-05-23 13:28:08 +0300

    Modify Tests in TestContentTypeSerializer

--- tests/serializers/test_contenttype.py
+++ tests/serializers/test_contenttype.py
@@ -4,9 +4,8 @@ Test for ContentTypeSerializer
 """
 from __future__ import unicode_literals
 
+from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase
-
-from model_mommy import mommy
 
 from tasking.serializers import ContentTypeSerializer
 
@@ -20,15 +19,11 @@ class TestContentTypeSerializer(TestCase):
         """
         Test that the serializer returns the expected fields
         """
-        mocked_task = mommy.make('tasking.Task')
-        data = {
-            'app_label': 'tasking',
-            'model': 'task',
-            'id': mocked_task.id,
-        }
+        mocked_contenttype = ContentType.objects.filter(
+            app_label='tasking', model='task').first()
 
-        serializer_instance = ContentTypeSerializer(data=data)
-        self.assertTrue(serializer_instance.is_valid())
+        serializer_data = ContentTypeSerializer(mocked_contenttype).data
+
         expected_fields = {
             'app_label',
             'model',
@@ -36,4 +31,7 @@ class TestContentTypeSerializer(TestCase):
         }
 
         self.assertEqual(set(expected_fields),
-                         set(list(serializer_instance.data.keys())))
+                         set(list(serializer_data.keys())))
+        self.assertEqual(mocked_contenttype.app_label, serializer_data['app_label'])
+        self.assertEqual(mocked_contenttype.model, serializer_data['model'])
+        self.assertEqual(mocked_contenttype.id, serializer_data['id'])
