e70978a3332b6a179b9fa686e76699957fa460f9
DavisRayM <davisraymondmuro@outlook.com>
2018-05-25 12:44:53 +0300

    Add Custom TaskManager

---
+++ tasking/models/managers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Manager module for Tasking
+"""
+from __future__ import unicode_literals
+
+from django.db import models
+
+class TaskManager(models.Manager):
+    """
+    Custom manager for Task
+    """
+
+    def get_queryset(self):
+        """
+        Custom get_query set for Task Model
+        """
+        queryset = super(TaskManager, self).get_queryset()
+        queryset = queryset.annotate(
+            submission_count=models.Count('submission__id'))
+        return queryset
