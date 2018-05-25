64a788a714ba64e06c642ea770d9fe0517bf07b3
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-05-25 15:30:23 +0300

    Add occurrences to urls

--- tasking/urls.py
+++ tasking/urls.py
@@ -6,18 +6,20 @@ from django.conf.urls import include, url
 
 from rest_framework import routers
 
-from tasking.viewsets import (LocationViewSet, ProjectViewSet,
-                              SegmentRuleViewSet, SubmissionViewSet,
-                              TaskViewSet, ContentTypeViewSet)
+from tasking.viewsets import (ContentTypeViewSet, LocationViewSet,
+                              ProjectViewSet, SegmentRuleViewSet,
+                              SubmissionViewSet, TaskOccurrenceViewSet,
+                              TaskViewSet)
 
 # pylint: disable=invalid-name
 router = routers.DefaultRouter(trailing_slash=False)
-router.register(r'segment-rules', SegmentRuleViewSet)
-router.register(r'tasks', TaskViewSet)
-router.register(r'submissions', SubmissionViewSet)
+router.register(r'contenttypes', ContentTypeViewSet)
 router.register(r'locations', LocationViewSet)
 router.register(r'projects', ProjectViewSet)
-router.register(r'contenttypes', ContentTypeViewSet)
+router.register(r'segment-rules', SegmentRuleViewSet)
+router.register(r'submissions', SubmissionViewSet)
+router.register(r'occurrences', TaskOccurrenceViewSet)
+router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
     url(r'^api/v1/', include(router.urls)),
