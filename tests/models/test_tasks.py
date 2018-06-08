5a43c71066d1ad71108660c954c74961c3cfb7e5
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-06-08 12:18:33 +0300

    Add tests for TaskLocation model

--- tests/models/test_tasks.py
+++ tests/models/test_tasks.py
@@ -22,3 +22,18 @@ class TestTasks(TestCase):
         cow_price = mommy.make('tasking.Task', name="Cow prices")
         expected = 'Cow prices - {}'.format(cow_price.pk)
         self.assertEqual(expected, six.text_type(cow_price))
+
+    def test_tasklocation_model_str(self):
+        """
+        Test __str__ on TaskLocation
+        """
+        self.assertEqual(
+            'oGame at home',
+            mommy.make(
+                'tasking.TaskLocation',
+                task=mommy.make('tasking.Task', name='oGame'),
+                location=mommy.make('tasking.Location', name='home'),
+                start='08:00:00',
+                end='19:00:00'
+            ).__str__()
+        )
