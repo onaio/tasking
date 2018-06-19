bb421df90f39140b3c1e36449c192b1ff94dd13b
DavisRayM <davisraymondmuro@outlook.com>
2018-06-19 14:17:33 +0300

    Add LocationType Serializer - Add tests for LocationType Serializer

---
+++ tasking/serializers/locationtype.py
@@ -0,0 +1,25 @@
+"""
+LocationType serializer
+"""
+from __future__ import unicode_literals
+
+from rest_framework import serializers
+
+from tasking.models import LocationType
+
+
+class LocationTypeSerializer(serializers.ModelSerializer):
+    """
+    Serializer for LocationType
+    """
+
+    # pylint: disable=too-few-public-methods
+    class Meta(object):
+        """
+        Meta Options for LocationTypeSerializer
+        """
+        model = LocationType
+        fields = [
+            'id',
+            'name',
+        ]
---
+++ tests/serializers/test_locationtype.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Test for Project Serializers
+"""
+from __future__ import unicode_literals
+
+from tests.base import TestBase
+
+from tasking.serializers import LocationTypeSerializer
+
+
+class TestLocationTypeSerializer(TestBase):
+    """
+    Test the LocationType Serializer
+    """
+
+    def test_create_locationtype(self):
+        """
+        Test that the serializer can create LocationType objects
+        """
+
+        data = {
+            'name': "Household",
+        }
+
+        serializer_instance = LocationTypeSerializer(data=data)
+        self.assertTrue(serializer_instance.is_valid())
+        locationtype = serializer_instance.save()
+
+        self.assertDictContainsSubset(data, serializer_instance.data)
+        self.assertEqual('Household', locationtype.name)
+
+        expected_fields = [
+            'id',
+            'name',
+        ]
+
+        self.assertEqual(set(expected_fields),
+                         set(list(serializer_instance.data.keys())))
