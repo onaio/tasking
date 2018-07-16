5a3ee47b2d88251cd3f6784b514f7696bcc4061f
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-07-16 16:09:34 +0300

    Fix pylint errors

--- tasking/common_tags.py
+++ tasking/common_tags.py
@@ -21,3 +21,5 @@ UNNECESSARY_FILE = _(
 )
 INVALID_START_DATE = _('The start date cannnot be greater than the end date')
 INVALID_END_DATE = _('The end date cannnot be lesser than the start date.')
+MISSING_START_DATE = _('Cannot determine the start date.  Please provide '
+                       'either the start date or timing rule(s)')
