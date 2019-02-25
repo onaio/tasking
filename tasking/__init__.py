8ce95cd8a5b74b44823f59e075329721b2b4eac6
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-02-25 11:58:02 +0300

    Autoload settings module

--- tasking/__init__.py
+++ tasking/__init__.py
@@ -6,3 +6,5 @@ from __future__ import unicode_literals
 
 VERSION = (0, 2, 1)
 __version__ = '.'.join(str(v) for v in VERSION)
+# pylint: disable=invalid-name
+default_app_config = 'tasking.apps.TaskingConfig'  # noqa
--- tasking/apps.py
+++ tasking/apps.py
@@ -3,7 +3,7 @@
 Apps module for tasking app
 """
 from __future__ import unicode_literals
-
+from django.utils.translation import ugettext_lazy as _
 from django.apps import AppConfig
 
 
@@ -12,3 +12,16 @@ class TaskingConfig(AppConfig):
     Tasking App Config Class
     """
     name = 'tasking'
+    app_label = "tasking"
+    verbose_name = _("Tasking")
+
+    def ready(self):
+        """
+        Do stuff when the app is ready
+        """
+        # set up app settings
+        from django.conf import settings
+        import tasking.settings as defaults
+        for name in dir(defaults):
+            if name.isupper() and not hasattr(settings, name):
+                setattr(settings, name, getattr(defaults, name))
