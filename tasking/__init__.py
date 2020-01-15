44f3481002e8088eae2df6ea254c6f9a774c02f3
Davis Raymond Muro <davisraymondmuro@outlook.com>
2020-01-15 17:44:50 +0300

    Bump version to 0.3.0

--- tasking/__init__.py
+++ tasking/__init__.py
@@ -2,7 +2,7 @@
 Main init file for tasking app
 """
 
-VERSION = (0, 2, 2)
+VERSION = (0, 3, 0)
 __version__ = '.'.join(str(v) for v in VERSION)
 # pylint: disable=invalid-name
 default_app_config = 'tasking.apps.TaskingConfig'  # noqa
